8cf5125cf71f0e9521f99b6598277b28f1a57c9b
YahirCC <147668023+VEGCRh@users.noreply.github.com>
2025-10-15 21:10:57 -0600

    resolviendo conflictos al intentar fusionar

--- funciones.js
+++ funciones.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-function saludar() { return 'Hola Mundo'; }
-=======
+
 function saludar() { return 'Hola Mundos'; }
->>>>>>> main
 function validarEmail(email) { return email.includes('@'); }
